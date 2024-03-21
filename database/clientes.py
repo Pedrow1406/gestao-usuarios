ed48450cc6a53e22d495dd923ded34636c33afe5
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-03-20 23:34:40 -0300

    terminando a parte de deletar usuario
    Aprendendo a usar o método delete. E criando a visualização de tabelas. Aprendendo a interceptar um formulário para não redirecionar a página após submissão

---
+++ database/clientes.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {'id':7,'nome': 'Pedrow', 'email':'pedro@hotmail.com'},
+    {'id':9,'nome': 'Lucas', 'email':'lucas@hotmail.com'},
+    {'id':11,'nome': 'Mateus', 'email':'mateus@hotmail.com'},
+    {'id':13,'nome': 'Joana', 'email':'joana@hotmail.com'},
+    {'id':14,'nome': 'Maria', 'email':'maria@hotmail.com'},
+    {'id':15,'nome': 'Vitor', 'email':'vitor@hotmail.com'},
+]
--- routes/clientes.py
+++ routes/clientes.py
@@ -1,35 +1,41 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request
+from database.clientes import CLIENTES
 cliente_route = Blueprint('cliente', __name__)
 
 @cliente_route.route('/')
 def lista_clientes(): # (GET) Exibe todos os clientes cadastrados
-    return "Todos os clientes"
+    return render_template('lista_clientes.html', usuarios=CLIENTES)
 
 @cliente_route.route('/<int:id_cliente>') # (GET) Exibe o cliente com base no id 
 def ver_cliente(id_cliente):
-    return f'{id_cliente}'
+    return render_template('cliente.html')
 
 
 @cliente_route.route('/new') # (GET) Vai exibir o formulario de cadastro de clientes 
 def form_cadastro_cliente():
-    return render_template('cliente.html')
+    return render_template('cadastro_cliente.html')
 
 @cliente_route.route('/', methods=["POST"]) # (POST) Vai receber os dados do formulario de clientes e vai validar e armazenar no database
 def cadastro_cliente():
-    return "Criando o usuário novo"
+    data = request.json
+    novo_usuario = {"id":CLIENTES[-1]['id'] + 1, "nome":data['username'],"email": data['email']}
+    CLIENTES.append(novo_usuario)
+    print(request.json)
+    return render_template('item_cliente.html', usuario=novo_usuario)
 
 @cliente_route.route('/edit') # (GET) Exibe o formulario para editar o cliente
 def form_update_cliente():
-    return 'Formulário de atualizar o cliente'
+    return render_template('edit_cliente.html')
 
 @cliente_route.route('/<int:id_cliente>', methods=['PUT']) # (PUT) Atualiza os dados de um cliente ja existente
 def update_cliente(id_cliente):
     return 'Atualiza os dados de um cliente ja existente'
 
-cliente_route.route('/<int:id_cliente>', methods=['DELETE']) # (DELETE) Deleta um cliente
+@cliente_route.route('/<int:id_cliente>', methods=['DELETE']) # (DELETE) Deleta um cliente
 def deletar_cliente(id_cliente):
-    return "Cliente Deletado"
+    global CLIENTES
+    CLIENTES = [cliente for cliente in CLIENTES if cliente['id'] != id_cliente]
+    return 'Usuário Deletado'
 
 
 
